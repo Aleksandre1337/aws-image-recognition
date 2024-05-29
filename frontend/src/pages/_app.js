306570b7b5c53df2643efa589b63c6f8a70bcc58
konstantine-exists <konstantine@exists.ai>
2024-05-29 20:35:26 +0400

    Ref: structure

---
+++ frontend/src/pages/_app.js
@@ -0,0 +1,11 @@
+import { MantineProvider } from '@mantine/core';
+import '@/styles/globals.css';
+import '@mantine/core/styles.css';
+
+export default function App({ Component, pageProps }) {
+	return (
+		<MantineProvider>
+			<Component {...pageProps} />
+		</MantineProvider>
+	);
+}
